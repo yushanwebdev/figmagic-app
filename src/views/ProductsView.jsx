ba6050705ef7c98700a5b0ddabe8f0be27f5f6a0
yushan <yushanwebdev@gmail.com>
2022-09-18 17:15:52 +0530

    feat: Setup - Remove final versions of the pages

--- src/views/ProductsView.jsx
+++ src/views/ProductsView.jsx
@@ -4,20 +4,24 @@ import PropTypes from 'prop-types';
 import productData from '../data.json';
 
 export const ProductsView = ({ itemCount, addItemToCart }) => {
-  const products = productData ? productData.products.map(product => {
-    const { imageUrl, heading, description, price, id} = product;
-    return <div key={id}>
-      <img src={imageUrl} width="320px" />
-      <h2>{heading}</h2>
-      <p>{description}</p>
-      <button id={`${id}#${price}#${heading}`} onClick={addItemToCart}>{price} $</button>
-    </div>
-  }) : null;
+  const products = productData
+    ? productData.products.map((product) => {
+        const { imageUrl, heading, description, price, id } = product;
+        return (
+          <div key={id}>
+            <img src={imageUrl} width="320px" />
+            <h2>{heading}</h2>
+            <p>{description}</p>
+            <button id={`${id}#${price}#${heading}`} onClick={addItemToCart}>
+              {price} $
+            </button>
+          </div>
+        );
+      })
+    : null;
 
   return (
     <>
-      <a href="/final/checkout">Switch to final</a>
-
       <main id="products">
         <h1>Products</h1>
 
@@ -30,7 +34,7 @@ export const ProductsView = ({ itemCount, addItemToCart }) => {
       </main>
       <footer>The ACME Corp. Potted Plants store</footer>
     </>
-  )
+  );
 };
 
 ProductsView.default = {
@@ -39,5 +43,5 @@ ProductsView.default = {
 
 ProductsView.propTypes = {
   itemCount: PropTypes.number.isRequired,
-	addItemToCart: PropTypes.func.isRequired
+  addItemToCart: PropTypes.func.isRequired
 };
